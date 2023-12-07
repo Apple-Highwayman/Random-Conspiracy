76896a1bb882ae834a9c03d99f9ee2ab1528d243
Apple-Highwayman <152738995+Apple-Highwayman@users.noreply.github.com>
2023-12-07 17:11:48 -0500

    Update RandomConspiracyDriver.java

--- src/randomGenerator/RandomConspiracyDriver.java
+++ src/randomGenerator/RandomConspiracyDriver.java
@@ -1,1 +1,20 @@
+package src.randomGenerator;
 
+
+public class RandomConspiracyDriver {
+
+    public static void main(String[] args) {
+
+        RandomConspiracy conspiracy = new RandomConspiracy(6820, 147);
+        System.out.println("Random Conspiracy Generator!");
+        System.out.println();
+        System.out.println("Culprit: " + conspiracy.object);
+        System.out.println();
+        System.out.println("Act: " + conspiracy.verb);
+        System.out.println();
+        System.out.println("Victim: " + conspiracy.subject);
+
+
+    } // main
+
+} // RandomConspiracy
